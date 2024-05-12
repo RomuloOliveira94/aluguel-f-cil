020ab52365315bc6a7cd4d8efdb3e83138a2233c
Rômulo Augusto <romuloffall@gmail.com>
2024-05-12 00:17:17 -0300

    #implement customer crud

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,56 @@
 class CustomersController < ApplicationController
+  before_action :load_customer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @customers = Customer.order(:name)
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to customers_path, notice: "Customer created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to customers_path, notice: "Customer updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    if @customer.destroy
+      redirect_to customers_path, notice: "Customer deleted successfully"
+    else
+      redirect_to customers_path, notice: "Customer could not be deleted"
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :dob, :mobile_phone)
+  end
+
+  def load_customer
+    @customer = Customer.find(params[:id]) if params[:id]
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :dob, presence: true
+  validates :mobile_phone, presence: true
 end
