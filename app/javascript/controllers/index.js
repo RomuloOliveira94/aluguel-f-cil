5e3e4ea004910c99d72626ea212c9dc30bb36b6d
Rômulo Augusto <romuloffall@gmail.com>
2024-06-09 13:08:46 -0300

    #create order equipament stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,6 +5,7 @@
 import { application } from "./application";
 import { Autocomplete } from "stimulus-autocomplete";
 
-import HelloController from "./hello_controller";
-application.register("hello", HelloController);
+import OrderEquipamentController from "./order_equipament_controller";
+
 application.register("autocomplete", Autocomplete);
+application.register("order-equipament", OrderEquipamentController);
---
+++ app/javascript/controllers/order_equipament_controller.js
@@ -0,0 +1,15 @@
+import { Autocomplete } from "stimulus-autocomplete";
+
+class OrderEquipamentController extends Autocomplete {
+  static targets = ["periodStart", "periodEnd"];
+
+  buildURL(query) {
+    console.log(this.periodStartTarget.value);
+    return `${new URL(
+      this.urlValue,
+      window.location.href
+    ).toString()}?q=${query}&period_start=${this.periodStartTarget.value}&period_end=${this.periodEndTarget.value}`;
+  }
+}
+
+export default OrderEquipamentController;
