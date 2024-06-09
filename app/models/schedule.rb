d1b23d176de92be1a023112d79f41cad147f302c
Rômulo Augusto <romuloffall@gmail.com>
2024-06-09 12:20:03 -0300

    #implement function to generate schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,5 @@ class Schedule < ApplicationRecord
   enum status: { pending: 'pending', in_progress: 'in_progress', done: 'done'}
 
   belongs_to :equipament
+  belongs_to :order, optional: true
 end
