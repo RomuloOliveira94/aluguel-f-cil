8b0258dc1f6be30a3ffdcc3161601494288c1bd6
Rômulo Augusto <romuloffall@gmail.com>
2024-06-09 13:22:44 -0300

    #implement orders index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @orders = Order.order(created_at: :desc)
+  end
+
   def new
     @order = Order.new
   end
